4fb894686d2b9b6055f52aed28f4dd90e1f1304b
Almaz Hadyrov <ahadyrov@gmail.com>
2024-06-03 19:10:01 +0300

    server-side encrypting removed

--- src/main/java/ru/almasgali/filesync_server/controller/FileController.java
+++ src/main/java/ru/almasgali/filesync_server/controller/FileController.java
@@ -2,8 +2,11 @@ package ru.almasgali.filesync_server.controller;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -23,7 +26,7 @@ public class FileController {
     private final StorageService storageService;
 
     @ResponseStatus(HttpStatus.OK)
-    @GetMapping
+    @GetMapping("/list")
     public List<FileResponse> getFiles(Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
         return storageService.loadAll(userDetails.getUsername());
@@ -32,18 +35,25 @@ public class FileController {
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void uploadFile(@RequestParam("file") MultipartFile file,
+                           @RequestParam("updated_at") long updatedAt,
                            Authentication authentication) {
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        storageService.store(file, userDetails.getUsername());
+        storageService.store(file, updatedAt, userDetails.getUsername());
     }
 
     @ResponseBody
-    @GetMapping("/{filename}")
-    public void getFile(@PathVariable String filename,
+    @GetMapping
+    public ResponseEntity<Resource> getFile(@RequestBody FileRequest request,
                         HttpServletResponse response,
                         Authentication authentication) {
 
         UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        storageService.writeFileToResponse(filename, userDetails.getUsername(), response);
+        Resource file = storageService.loadAsResource(request.getName(), userDetails.getUsername());
+
+        if (file == null)
+            return ResponseEntity.notFound().build();
+
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
     }
 }
--- src/main/java/ru/almasgali/filesync_server/service/StorageService.java
+++ src/main/java/ru/almasgali/filesync_server/service/StorageService.java
@@ -1,7 +1,7 @@
 package ru.almasgali.filesync_server.service;
 
-import jakarta.servlet.http.HttpServletResponse;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 import org.springframework.util.FileSystemUtils;
@@ -10,36 +10,25 @@ import ru.almasgali.filesync_server.data.dto.FileResponse;
 import ru.almasgali.filesync_server.exceptions.storage.StorageException;
 import ru.almasgali.filesync_server.exceptions.storage.StorageFileNotFoundException;
 
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.NoSuchPaddingException;
-import javax.crypto.SecretKey;
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
 @Service
 public class StorageService {
 
-    private final Path rootLocation = Path.of("/storage");
-    private final Cipher cipher;
-    private SecretKey secretKey;
+    private final Path rootLocation = Path.of("./storage");
 
     public StorageService() {
-        try {
-            cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-            throw new StorageException("Could not initialize storage", e);
-        }
         init();
     }
 
-    public void store(MultipartFile file, String username) {
+    public void store(MultipartFile file, long updatedAt, String username) {
         try {
             if (file.isEmpty()) {
                 throw new StorageException("Failed to store empty file.");
@@ -49,15 +38,12 @@ public class StorageService {
                     .resolve(Paths.get(file.getOriginalFilename()))
                     .normalize().toAbsolutePath();
             Files.createDirectories(destinationFile);
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            try (CipherInputStream cis = new CipherInputStream(file.getInputStream(), cipher)) {
-                Files.copy(cis, destinationFile,
+            try (InputStream is = file.getInputStream()) {
+                Files.copy(is, destinationFile,
                         StandardCopyOption.REPLACE_EXISTING);
             }
         } catch (IOException e) {
             throw new StorageException("Failed to store file.", e);
-        } catch (InvalidKeyException e) {
-            throw new StorageException("Invalid secret key.", e);
         }
     }
 
@@ -85,20 +71,19 @@ public class StorageService {
         return rootLocation.resolve(username).resolve(filename);
     }
 
-    public void writeFileToResponse(String filename, String username, HttpServletResponse response) {
+    public Resource loadAsResource(String filename, String username) {
         try {
             Path file = load(filename, username);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            response.setContentType("text/plain");
-            response.setHeader("Content-Disposition", "filename=\""+ filename + "\"");
-            try (CipherInputStream cis = new CipherInputStream(Files.newInputStream(file), cipher);
-                 OutputStream os = response.getOutputStream()) {
-                IOUtils.copy(cis, os);
+            Resource resource = new UrlResource(file.toUri());
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new StorageFileNotFoundException(
+                        "Could not read file: " + filename);
+
             }
-        } catch (IOException e) {
-            throw new StorageFileNotFoundException("Could not read file: " + filename, e);
-        } catch (InvalidKeyException e) {
-            throw new StorageException("Invalid secret key.", e);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
         }
     }
 
@@ -109,12 +94,6 @@ public class StorageService {
     public void init() {
         try {
             Files.createDirectories(rootLocation);
-            Path path = Path.of("/storage/secret.key");
-            try (ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(path))) {
-                secretKey = (SecretKey) ois.readObject();
-            } catch (ClassNotFoundException e) {
-                throw new StorageException("Could not initialize storage", e);
-            }
         } catch (IOException e) {
             throw new StorageException("Could not initialize storage", e);
         }
